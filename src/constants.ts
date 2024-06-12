df40aa0a376367576a34c9f83148ef13b9c5e617
Richard Liu <richard.y.liu@berkeley.edu>
2024-06-12 15:51:49 -0700

    add esmeralda tickets

--- src/constants.ts
+++ src/constants.ts
@@ -21,12 +21,11 @@ export const EVERYONE_SEMAPHORE_GROUP_URL = IS_PROD
   ? "https://api-staging.zupass.org/semaphore/5"
   : "http://localhost:3002/semaphore/5";
 
-  export const ticketTypeNames = [
-    "Zuzalu",
-    "ZuConnect",
-    "Vitalia",
-    "ZuVillage",
-    // "Esmeralda",
-    "AgoraCore"
-  ] as const;
-  
+export const ticketTypeNames = [
+  "Zuzalu",
+  "ZuConnect",
+  "Vitalia",
+  "ZuVillage",
+  "Esmeralda",
+  "AgoraCore"
+] as const;
--- src/zupass-config.ts
+++ src/zupass-config.ts
@@ -1,5 +1,6 @@
-import { ticketTypeNames } from './constants';
-import { TicketTypeName } from './types';
+import { ESMERALDA_TICKET } from "@pcd/zuauth/configs/esmeralda";
+import { ticketTypeNames } from "./constants";
+import { TicketTypeName } from "./types";
 
 /**
  * We want to match a ticket based on a pairing of event IDs and product IDs.
@@ -13,12 +14,22 @@ import { TicketTypeName } from './types';
  */
 export const whitelistedTickets: Record<
   TicketTypeName,
-  Array<{ eventId: string; productId: string, eventName?: string, productName?: string, pcdType?: string, publicKey: [string, string] }>
+  Array<{
+    eventId: string;
+    productId: string;
+    eventName?: string;
+    productName?: string;
+    pcdType?: string;
+    publicKey: [string, string];
+  }>
 > = {
   Zuzalu: [
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e","29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"],
+      publicKey: [
+        "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+        "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
+      ],
       eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
       eventName: "Zuzalu",
       productId: "5ba4cd9e-893c-4a4a-b15b-cf36ceda1938",
@@ -26,7 +37,10 @@ export const whitelistedTickets: Record<
     },
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e","29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"],
+      publicKey: [
+        "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+        "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
+      ],
       eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
       eventName: "Zuzalu",
       productId: "10016d35-40df-4033-a171-7d661ebaccaa",
@@ -34,7 +48,10 @@ export const whitelistedTickets: Record<
     },
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e","29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"],
+      publicKey: [
+        "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+        "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
+      ],
       eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
       eventName: "Zuzalu",
       productId: "53b518ed-e427-4a23-bf36-a6e1e2764256",
@@ -44,7 +61,10 @@ export const whitelistedTickets: Record<
   ZuConnect: [
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e","29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"],
+      publicKey: [
+        "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+        "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
+      ],
       eventId: "91312aa1-5f74-4264-bdeb-f4a3ddb8670c",
       eventName: "ZuConnect",
       productId: "cc9e3650-c29b-4629-b275-6b34fc70b2f9",
@@ -52,7 +72,10 @@ export const whitelistedTickets: Record<
     },
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e","29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"],
+      publicKey: [
+        "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+        "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
+      ],
       eventId: "54863995-10c4-46e4-9342-75e48b68d307",
       eventName: "ZuConnect",
       productId: "d2123bf9-c027-4851-b52c-d8b73fc3f5af",
@@ -60,7 +83,10 @@ export const whitelistedTickets: Record<
     },
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e","29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"],
+      publicKey: [
+        "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+        "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
+      ],
       eventId: "797de414-2aec-4ef8-8655-09df7e2b6cc6",
       eventName: "ZuConnect",
       productId: "d3620f38-56a9-4235-bea8-0d1dba6bb623",
@@ -68,7 +94,10 @@ export const whitelistedTickets: Record<
     },
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e","29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"],
+      publicKey: [
+        "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+        "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
+      ],
       eventId: "f7370f63-b9ae-480c-9ded-0663f1922bef",
       eventName: "ZuConnect",
       productId: "0179ed5b-f265-417c-aeaa-ac61a525c6b0",
@@ -78,7 +107,10 @@ export const whitelistedTickets: Record<
   Vitalia: [
     {
       pcdType: "eddsa-ticket-pcd",
-      publicKey: ["0d3388a18b89dd012cb965267ab959a6ca68f7e79abfdd5de5e3e80f86821a0d", "0babbc67ab5da6c9245137ae75461f64a90789ae5abf3737510d5442bbfa3113"],
+      publicKey: [
+        "0d3388a18b89dd012cb965267ab959a6ca68f7e79abfdd5de5e3e80f86821a0d",
+        "0babbc67ab5da6c9245137ae75461f64a90789ae5abf3737510d5442bbfa3113"
+      ],
       eventId: "9ccc53cb-3b0a-415b-ab0d-76cfa21c72ac",
       eventName: "Vitalia",
       productId: "cd3f2b06-e520-4eff-b9ed-c52365c60848",
@@ -88,24 +120,16 @@ export const whitelistedTickets: Record<
   ZuVillage: [
     {
       productId: "aecf9f84-b92f-5b40-8541-cbb48f4d6267",
-      publicKey: ["1ebfb986fbac5113f8e2c72286fe9362f8e7d211dbc68227a468d7b919e75003","10ec38f11baacad5535525bbe8e343074a483c051aa1616266f3b1df3fb7d204"],
+      publicKey: [
+        "1ebfb986fbac5113f8e2c72286fe9362f8e7d211dbc68227a468d7b919e75003",
+        "10ec38f11baacad5535525bbe8e343074a483c051aa1616266f3b1df3fb7d204"
+      ],
       eventId: "6f5f194b-97b5-5fe9-994d-0998f3eacc75",
       eventName: "ZuVillage Georgia",
       productName: "Contributor"
     }
   ],
-  // Esmeralda: [
-  //   {
-  //     pcdType: "eddsa-ticket-pcd",
-  //     publicKey: [
-  //       "1ebfb986fbac5113f8e2c72286fe9362f8e7d211dbc68227a468d7b919e75003",
-  //       "10ec38f11baacad5535525bbe8e343074a483c051aa1616266f3b1df3fb7d204"
-  //     ],
-  //     eventId: "21c7db2e-08e3-4234-9a6e-386a592d63c8",
-  //     eventName: "Edge Esmeralda",
-  //     productId: ''
-  //   }
-  // ],
+  Esmeralda: ESMERALDA_TICKET,
   AgoraCore: [
     {
       pcdType: "eddsa-ticket-pcd",
@@ -133,8 +157,8 @@ export const whitelistedTickets: Record<
 };
 
 // Map the above data structure into a simple array of event IDs.
-export const supportedEvents = Object.values(whitelistedTickets).flatMap((items) =>
-  items.map((item) => item.eventId)
+export const supportedEvents = Object.values(whitelistedTickets).flatMap(
+  (items) => items.map((item) => item.eventId)
 );
 
 /**
